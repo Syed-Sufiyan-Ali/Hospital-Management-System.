3e267d2b54bf77168b7571a4e9a8b8b2f20669f8
Syed Sufiyan Ali <syedsufiyanali2007890@gmail.com>
2026-06-07 12:44:09 +0500

    saving the data in array varibles

--- Hospital.cpp
+++ Hospital.cpp
@@ -1,48 +1,68 @@
 #include <iostream>
 using namespace std;
-//struct patient {
-//	
-//	string name , gender , illness  ;
-//	int age , id  ;
-//};
-int main (){
- string name , illness ;
- char gender ;
- int age , id , user_response , Addmited_For ;
+struct Patient {
 
- cout << " ===== HOSPITAL MANAGEMENT SYSTEM ===== \n\n1. Add Patient \n2. View Patients\n3. Generate Bill\n4. Exit \n" ;
+    string name;
+    string illness;
+    char gender;
+
+    int age;
+    int id;
+    
+
+};
+Patient patient [10];
+
+int main (){
+ int user_response;
+ int total_patient = 0 ;
+ int Addmited_For;
+ while (true){
+ cout << " ===== HOSPITAL MANAGEMENT SYSTEM ===== \n\n1. Add Patient \n2. View Patients\n3. Generate Bill\n4. dfa ho \n" ;
  
  cin >> user_response;
+
  if (user_response == 1){
  	cout << "Enter patient name: ";
- 	cin >> name;
+ 	cin >> patient[total_patient].name;
  	cout << "Enter patient age: ";
- 	cin >> age;
+ 	cin >> patient[total_patient].age;
  	cout << "Enter patient gender (press M or F ): ";
- 	cin >> gender;
+ 	cin >> patient[total_patient].gender;
  	cout << "Enter patient ID: ";
- 	cin >> id;
+ 	cin >> patient[total_patient].id;
  	cout << "Enter illness: ";
- 	cin >> illness;
+ 	cin >> patient[total_patient].illness;
  	
  }
+ 
+
  else  if (user_response == 2){
     cout << "Enter the number of Days patient has been addmitted";
-    cin << Addmited_for;
+    cin >> Addmited_For;
  	cout << "Enter patient ID: ";
- 	cin >> id;
+ 	cin >> patient[total_patient].id;
  	cout << "Enter illness: ";
- 	cin >> illness;
- 	Cout << "The Patient " << name << "has been addmitted for " << Addmited_for << " days";
+ 	cin >> patient[total_patient].illness;
+ 	cout << "The Patient " << patient[total_patient].name << endl << "ID: " << patient[total_patient].id << endl << "Addmitted for " << Addmited_For << " days\n ";
  }
-;
-else  if (user_response == 3){
- 	cout << "Enter illness";
- 	cin >> illness ;
- 	cout << "Enter the number of days addmited";
- 	cin >> Addmited_for ;
- 	Cout << "This Sums up your bill to " << Addmited_for * 1000;
- 	
 
+else if (user_response == 3){
+ 	cout << "Enter illness: ";
+ 	cin >> patient[total_patient].illness;
+ 	cout << "Enter the number of days addmited";
+ 	cin >> Addmited_For ;
+ 	cout << "This Sums up your bill to " << Addmited_For * 1000;
  }
+ else if (user_response == 4){
+	cout << "You have exited the program. Thank you for using it\n";
+	break;
+ }
+ else {
+ cout << "Invalid response. Please try again.\n";
+ }
+ 
+}
+total_patient++;
+
 }
