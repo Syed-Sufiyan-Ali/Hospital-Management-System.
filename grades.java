37924d092465590c45c755b00c784139ec0d76dc
Syed Sufiyan Ali <syedsufiyanali2007890@gmail.com>
2026-06-09 17:26:17 +0500

    Initial Java project upload

---
+++ grades.java
@@ -0,0 +1,27 @@
+public class grades {
+    public static void main (String[] args){
+        int marking = 41;
+        if (marking >= 90 ){
+            System.out.println("A+");
+        }
+        else if (marking >= 80){
+            System.out.println("A");
+        }
+        else if (marking >= 70){
+            System.out.println("B");
+        }
+        else if (marking >= 60){
+            System.out.println("C");
+        }
+        else if (marking >= 50){
+            System.out.println("D");
+        }
+        else if (marking >= 40){
+            System.out.println("E");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+    
+}
