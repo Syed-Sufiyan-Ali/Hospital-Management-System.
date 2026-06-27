5af525c2ec20db5b1f8d7e62424365379fa94d9b
Syed Sufiyan Ali <syedsufiyanali2007890@gmail.com>
2026-06-27 23:08:42 +0500

    adding an authentication method

--- login.h
+++ login.h
@@ -1,13 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
-class Login {
- private :
-    string username = "ManageSystem";
-    string password = "password123";
- public :
-    bool authenticate(string U , string P) {
-       return (U == username && P == password);
 
+class Login
+{
+private:
+    string username;
+    string password;
+
+public:
+    Login()
+    {
+        ifstream file("userpass.txt");
+
+        if (file.is_open())
+        {
+            string line;
+
+            getline(file, line);
+
+            stringstream ss(line);
+
+            getline(ss, username, '|');
+            getline(ss, password);
+
+            file.close();
+        }
     }
 
+    bool authenticate(string input_username, string input_password)
+    {
+        if (input_username == username &&
+            input_password == password)
+        {
+            return true;
+        }
+
+        return false;
+    }
 };
